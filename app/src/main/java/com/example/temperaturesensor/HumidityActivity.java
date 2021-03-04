5792616424433800315eec27624a1100bd5bff0d
migue <miguelangelosilvacarvalho96@gmail.com>
2021-03-04 12:20:42 +0000

    melhoria no HumidityActivity

--- app/src/main/java/com/example/temperaturesensor/HumidityActivity.java
+++ app/src/main/java/com/example/temperaturesensor/HumidityActivity.java
@@ -1,6 +1,7 @@
 package com.example.temperaturesensor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,7 +18,7 @@ public class HumidityActivity extends Activity implements SensorEventListener {
     private Sensor mTemperatureSensor;
     private  boolean isHumiditySensorPresent;
     private boolean isTemperaturePresent;
-    private TextView isRelativeHumidityValue;
+    private TextView mRelativeHumidityValue;
     private  TextView mAbsoluteHumidityValue;
     private TextView mDewPointValue;
     private  float mLasKnownRelativeHumidity = 0;
@@ -26,6 +27,27 @@ public class HumidityActivity extends Activity implements SensorEventListener {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.humidity_layout);
+        mRelativeHumidityValue = (TextView)findViewById(R.id.relativehumiditytext);
+        mAbsoluteHumidityValue = (TextView)findViewById(R.id.absolutehumiditytext);
+        mSensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null){
+            mHumiditySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+            isHumiditySensorPresent = true;
+        }else {
+            mRelativeHumidityValue.setText("Relative Humidty Sensor is not available!");
+            mAbsoluteHumidityValue.setText("Cannot calculate Absolute Humity as relative humity sensor is not available!");
+            mDewPointValue.setText("Cannot calculate Dew Point, as relative humity sensor is not available!");
+            isHumiditySensorPresent = false;
+        }
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null){
+            mTemperatureSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            isTemperaturePresent = true;
+        } else {
+            mAbsoluteHumidityValue.setText("Cannot calculate Absolute Humidity, as temperature sensor is not available!");
+            mDewPointValue.setText("Cannot calculate Dew Point, as temperature sensor is not available!");
+            isTemperaturePresent = false;
+        }
+
     }
 
     @Override
